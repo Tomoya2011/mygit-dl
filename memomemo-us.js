88d62146c1c31733533d401d7c0958687b02d9db
Tomoya108 <83895391+Tomoya108@users.noreply.github.com>
2021-05-10 16:05:35 +0900

    Create memomemo-us.js

---
+++ memomemo-us.js
@@ -0,0 +1,8 @@
+function MoveCheck() {
+	if( confirm("現在、memomemoは日本語版のみしかございません。日本語版でも宜しい方は、OKをクイックしてください。") ) {
+		window.location.href = "https://github.com/Tomoya108/memomemo/releases/tag/1.2.0";
+	}
+	
+	
+
+}
